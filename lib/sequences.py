bc96d9051408e79121133aab4fa3a2cebb41611e
Monalisa Onyango <+>
2024-08-28 14:50:26 +0300

    sequence lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+
+
+    # fibonacci_sequence = [0,1]
+
+    # while len(fibonacci_sequence) < length:
+    #     next_value = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+    #     fibonacci_sequence.append(next_value)
+
+    #     print(fibonacci_sequence[:length])
+
+    fibonacci_sequence =[]
+
+    if length > 0:
+        fibonacci_sequence.append(0)
+
+    if length >= 2:
+        fibonacci_sequence.append(1)
+
+        for i in range(2, length):
+            fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+    
+    print(fibonacci_sequence)
+
+
+print_fibonacci(10)
